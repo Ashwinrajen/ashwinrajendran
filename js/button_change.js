e3f5a17d862dd8cd2005b07dfff7067374bec502
ashiwin <ashiwin@lionsbot.com>
2022-12-31 04:45:34 +0800

    new interface

--- js/button_change.js
+++ js/button_change.js
@@ -46,15 +46,4 @@ document.getElementById("expandMedia3DVision").addEventListener(
   false
 );
 
-document.getElementById("expandMediaSensorFusion").addEventListener(
-  "click",
-  function(event) {
-    if (event.target.value === "Show Media") {
-      event.target.value = "Hide Media";
-    } else {
-      event.target.value = "Show Media";
-    }
-  },
-  false
-);
 
---
+++ js/button_change_2.js
@@ -0,0 +1,23 @@
+document.getElementById("expandMediaSensorFusion").addEventListener(
+  "click",
+  function(event) {
+    if (event.target.value === "Show Media") {
+      event.target.value = "Hide Media";
+    } else {
+      event.target.value = "Show Media";
+    }
+  },
+  false
+);
+
+document.getElementById("expandRoboticProjects").addEventListener(
+  "click",
+  function(event) {
+    if (event.target.value === "Show Media") {
+      event.target.value = "Hide Media";
+    } else {
+      event.target.value = "Show Media";
+    }
+  },
+  false
+);
